112bf8c8a3272d05908769a8f8699d881828bd3d
rsnazario <rafael.s.nazario@gmail.com>
2020-10-06 16:43:38 -0300

    startBattle on BattleScene

--- js/battle.js
+++ js/battle.js
@@ -1,5 +1,37 @@
 class BattleScene extends Phaser.Scene {
+  constructor() {
+    super('BattleScene');
+  }
 
+  create() {
+    this.cameras.main.setBackgroundColor('rgba(0, 200, 0, 0.5)');
+    this.startBattle();
+    this.sys.events.on('wake', this.startBattle, this);
+  }
+
+  startBattle() {
+    
+    var warrior = new PlayerCharacter(this, 250, 50, 'player', 1, 'Warrior', 100, 20);
+    this.add.existing(warrior);
+
+    var mage = new PlayerCharacter(this, 250, 100, 'player', 4, 'Mage', 80, 8);
+    this.add.existing(mage);
+
+    var dragonblue = new Enemy(this, 50, 40, 'dragonblue', null, 'Dragon', 50, 1);
+    this.add.existing(dragonblue);
+
+    var dragonorange = new Enemy(this, 50, 40, 'dragonorange', null, 'Dragon2', 50, 1);
+    this.add.existing(dragonorange);
+
+    this.heroes = [warrior, mage],
+    this.enemies = [dragonblue, dragonorange];
+
+    this.units = this.heroes.concat(this.enemies);
+
+    this.index = -1;
+
+    this.scene.run('UIScene');
+  }
 }
 
 class UIScene extends Phaser.Scene {
