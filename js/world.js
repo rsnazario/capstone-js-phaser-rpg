77947d4d276cfdd9d5cb44f7b3f1378272a4a693
rsnazario <rafael.s.nazario@gmail.com>
2020-10-06 15:23:11 -0300

    First Version Create Method for WorldScene

--- js/world.js
+++ js/world.js
@@ -18,3 +18,66 @@ class BootScene extends Phaser.Scene {
   }
 };
 
+class WorldScene extends Phaser.Scene {
+  constructor() {
+    super('WorldScene');
+  }
+
+  preload() {
+
+  }
+
+  create() {
+    var map = this.make.tilemap({key: 'map'});
+    var tiles = map.addTilesetImage('spritesheet', 'tiles');
+
+    var grass = map.createStaticLayer('Grass', tiles, 0, 0);
+    var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
+    obstacles.setCollisionByExclusion(-1);
+
+    this.player = this.physics.add.sprite(50, 100, 'player', 6);
+
+    this.physics.world.bounds.width = map.widthInPixels;
+    this.physics.world.bounds.height = map.heightInPixels;
+    this.player.setCollideWorldBounds(true);
+    this.physics.add.collider(this.player, obstacles);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player);
+    this.cameras.main.roundPixels = true;
+
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('player', {frames: [1, 7, 1, 13]}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('player', {frames: [1, 7, 1, 13]}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'up',
+      frames: this.anims.generateFrameNumbers('player', {frames: [2, 8, 2, 14]}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: 'down',
+      frames: this.anims.generateFrameNumbers('player', {frames: [0, 6, 0, 12]}),
+      frameRate: 10,
+      repeat: -1
+    });
+
+  }
+
+  
+}
