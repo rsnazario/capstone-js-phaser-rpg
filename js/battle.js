3c5f599f2b6e08abcf182f004784ab7d6ec62cdc
rsnazario <rafael.s.nazario@gmail.com>
2020-10-06 15:48:29 -0300

    Schematic for BattleScene

---
+++ js/battle.js
@@ -0,0 +1,43 @@
+class BattleScene extends Phaser.Scene {
+
+}
+
+class UIScene extends Phaser.Scene {
+
+}
+
+class Unit extends Phaser.GameObjects.Sprite {
+
+}
+
+class PlayerCharacter extends Unit {
+
+}
+
+class Enemy extends Unit {
+
+}
+
+class Menu extends Phaser.GameObjects.Container {
+
+}
+
+class MenuItem extends Phaser.GameObjects.Text {
+
+}
+
+class HeroesMenu extends Menu {
+
+}
+
+class ActionsMenu extends Menu {
+
+}
+
+class EnemiesMenu extends Menu {
+
+}
+
+class Message extends Phaser.GameObjects.Container {
+
+}
