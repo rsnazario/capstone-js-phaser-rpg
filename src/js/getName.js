de19cde18ea3ce98c86a91b545643b7badd58376
rsnazario <rafael.s.nazario@gmail.com>
2020-10-09 16:23:26 -0300

    getName and start game on ENTER KEY push

--- src/js/getName.js
+++ src/js/getName.js
@@ -20,10 +20,16 @@ export default class GetName extends Phaser.Scene {
       if (event.keyCode === 8 && textEntry.length > 0) {
         textEntry = textEntry.substr(0, textEntry.length - 1);
       }
-      else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 92)) {
-        textEntry += event.key;
+      else if (event.keyCode === 32 || (event.keyCode >= 48 && event.keyCode < 106)) {
+        if (textEntry.length <= 15) {
+          textEntry += event.key;
+        }
       }
       this.text.setText(textEntry);
+      if (event.keyCode === 13 && textEntry.length > 0) {
+        window.playerName = textEntry;
+        this.scene.start('WorldScene');
+      }
     });
 
     // console.log(textEntry.text);
