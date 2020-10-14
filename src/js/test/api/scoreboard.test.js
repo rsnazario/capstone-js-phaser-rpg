8a767429d782dddb9061af89bad004bff77fee75
rsnazario <rafael.s.nazario@gmail.com>
2020-10-14 15:26:06 -0300

    Added API Tests

---
+++ src/js/test/api/scoreboard.test.js
@@ -0,0 +1,38 @@
+import { TestScheduler } from 'jest';
+import scoreboard from '../../api/scoreboard';
+
+const axios = require('axios');
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const key = 't82Jd3rsSodrA4KcbQ65';
+
+jest.mock('axios');
+
+describe('API Working', () => {
+
+  test('API Fetches Results', () => {
+    const mockedResponse = { data: { user: 'user', score: 10} };
+    axios.get.mockResolvedValue(mockedResponse);
+    scoreboard.getScore();
+
+    expect(axios.get).toHaveBeenCalled();
+    expect(axios.get).toHaveBeenCalledWith(`${url}${key}/scores`);
+  });
+});
+
+describe('API Helper: Reorder', () => {
+  test('Reorder in Descending Order', () => {
+    const mockedResponse = [
+      {user: 'user1', score: 10},
+      {user: 'user2', score: 50},
+      {user: 'user3', score: 30}
+    ];
+    let result = scoreboard.orderedScores(mockedResponse);
+    let expectedResult = [
+      {score: 50, user: 'user2'},
+      {score: 30, user: 'user3'},
+      {score: 10, user: 'user1'}
+    ]
+
+    expect(result).toEqual(expectedResult);
+  });
+});
